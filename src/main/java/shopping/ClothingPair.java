76b2755f4a0aa6362c07deaf4bbf69ce05ffa395
Simon Roberts <simon@dancingcloudservices.com>
2020-09-01 09:36:38 -0600

    Final, with arrays and bridge methods examples

--- src/main/java/shopping/ClothingPair.java
+++ src/main/java/shopping/ClothingPair.java
@@ -10,6 +10,9 @@ public class ClothingPair<E extends Object & Colored & Sized> extends Pair<E> {
 
   // send command argument here.
   public boolean isMatched() {
+//    E targetLeft = left;
+  // could be.. (isn't) Sized & Colored
+//    var targetLeft = getLeft();
 //    if (left instanceof Sock && right instanceof Sock) // YUK!!!!
 //    if (left instanceof Colored && right instanceof Colored) // still YUK!!!!
     return left.getColor().equals(right.getColor())
--- src/main/java/shopping/Shop.java
+++ src/main/java/shopping/Shop.java
@@ -12,5 +12,10 @@ public class Shop {
 //        + ClothingPair.<Sock>match(new Sock(10, "Blue"), new Sock(11, "Blue")));
         + ClothingPair.match(new Sock(10, "Blue"), new Sock(11, "Blue")));
     //    ClothingPair<String> cps;
+
+//    ClothingPair cp = ps;
+    // if context is "vague enough" we actually canget
+    // a type for s that is Sized & Colored
+    var s = ps.getLeft();
   }
 }
